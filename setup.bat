ef3f84d7cc0d80aa7f620b91b11b5e3b35ef81ba
Vodkarm06 <79284263+Vodkarm@users.noreply.github.com>
2021-08-10 15:10:11 +0200

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+title PyPackager Installer
+cls
+pip install -r requirements.txt
+echo Installation finished, you can now use VNitro ! Check Github page for learn how to use, Enjoy with it ! :)
+pause
