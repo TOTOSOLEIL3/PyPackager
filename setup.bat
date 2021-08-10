c129344ccecc17ccb16bab119429a45f79c306b0
Vodkarm06 <79284263+Vodkarm@users.noreply.github.com>
2021-08-10 22:59:34 +0200

    Delete setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title PyPackager Installer
-cls
-pip install -r requirements.txt
-echo Installation finished, you can now use VNitro ! Check Github page for learn how to use, Enjoy with it ! :)
-pause
